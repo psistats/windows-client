e5e7f569762fd9b87646d2ad1190d30a8779a40e
'alex-dow' <'adow@psikon.com'>
2014-09-30 23:05:49 -0400

    write to version file and replace sonar version

--- build-removedev.py
+++ build-removedev.py
@@ -7,8 +7,12 @@ NEW_VERSION = VERSION.replace("-dev", "")
 
 with open("sonar-project.properties") as f:
     contents = f.read()
-    contents.replace("sonar.projectVersion=" + VERSION, "sonar.projectVersion=" + NEW_VERSION)
+    contents = contents.replace("sonar.projectVersion=" + VERSION, "sonar.projectVersion=" + NEW_VERSION)
     
 with open("sonar-project.properties", "w") as f:
     f.write(contents)
+    
+with open("VERSION", "w") as f:
+    f.write(NEW_VERSION)
+
 
