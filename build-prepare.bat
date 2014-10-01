e1f71e301a9eb3b4598cfe394ec1e438f1cff46e
'alex-dow' <'adow@psikon.com'>
2014-10-01 00:10:15 -0400

    remove push

--- build-prepare.bat
+++ build-prepare.bat
@@ -5,9 +5,6 @@ if errorlevel 1 goto failed
 git commit -am "Removing dev from version numbers"
 if errorlevel 1 goto failed
 
-git push
-if errorlevel 1 goto failed
-
 call build.bat debug
 if errorlevel 1 goto failed
 
