c89247d4475750338edc61b582bf713fb2e53fb3
'alex-dow' <'adow@psikon.com'>
2014-10-01 00:38:02 -0400

    revert versions to -dev

--- build-prepare.bat
+++ build-prepare.bat
@@ -2,13 +2,13 @@
 call python build-removedev.py
 if errorlevel 1 goto failed
 
-git commit -am "Removing dev from version numbers"
-if errorlevel 1 goto failed
-
 call build.bat debug
 if errorlevel 1 goto failed
 
 call build.bat release
+if errorlevel 1 goto failed
+
+git commit -am "Removing dev from version numbers"
 if errorlevel 1 goto failed
 
 goto:eof
