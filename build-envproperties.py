22d7f8b34cad208938c51f847fb912006163e8b6
'alex-dow' <'adow@psikon.com'>
2014-10-09 17:02:29 -0400

    Adjusted build scripts

--- build-envproperties.py
+++ build-envproperties.py
@@ -3,7 +3,10 @@ import os
 
 artifact = "ARTIFACT_ID=psistats-client"
 version = "VERSION=" + f.open("VERSION").read()
+build_number = "BUILD_NUMBER=" + os.environ['BUILD_NUMBER']
 
 with f.open("env.properties", "w")):
     f.write(artifact + "\n")
-    f.write(version)
+    f.write(version + "\n")
+    f.write(build_number)
+    
