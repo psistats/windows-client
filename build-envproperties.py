7770ddebd1a25c651589e0ea6e0823e3690aa886
'alex-dow' <'adow@psikon.com'>
2014-10-09 17:07:46 -0400

    fixed typo in build-envproperties.py

--- build-envproperties.py
+++ build-envproperties.py
@@ -5,7 +5,7 @@ artifact = "ARTIFACT_ID=psistats-client"
 version = "VERSION=" + open("VERSION").read()
 build_number = "BUILD_NUMBER=" + os.environ['BUILD_NUMBER']
 
-with f.open("env.properties", "w") as f:
+with open("env.properties", "w") as f:
     f.write(artifact + "\n")
     f.write(version + "\n")
     f.write(build_number)
